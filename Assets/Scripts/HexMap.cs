d91644f554c760c316fe90c516ffa6670a83b680
Dirk Schut <dirk@mrdls.com>
2017-05-05 18:16:41 +0200

    Initial simple hexmap

---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+
+	/// <summary>
+    /// Instatiate the HexMap script
+    /// </summary>
+	void Start ()
+    {
+        GenerateMap();
+	}
+
+    public GameObject HexPrefab;
+
+    /// <summary>
+    /// Generates the Map
+    /// </summary>
+	public void GenerateMap()
+    {
+        for (int col = 0; col < 10; col++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                GameObject HexGO = Instantiate(HexPrefab, new Vector3(col, 0, row), Quaternion.identity, this.transform);
+            }
+        }
+    }
+}
